0abe7bae7222be0064abfc3b61f839b9e595cc1f
Jason Meller <jason@kolide.co>
2020-10-24 21:32:18 -0400

    Do not blindly retry

--- lib/pipedrive/base.rb
+++ lib/pipedrive/base.rb
@@ -19,14 +19,7 @@ module Pipedrive
 
       url = build_url(args, params.delete(:fields_to_select))
       params = params.to_json unless method.to_sym == :get
-      begin
-        res = connection.__send__(method.to_sym, url, params)
-      rescue Errno::ETIMEDOUT
-        retry
-      rescue Faraday::ParsingError
-        sleep 5
-        retry
-      end
+      res = connection.__send__(method.to_sym, url, params)
       process_response(res)
     end
 
